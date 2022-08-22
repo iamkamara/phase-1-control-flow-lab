936c9e6ffcc62db2ea2b75f107c8fdb36660b853
onesmus <onesmusqamara@gmail.com>
2022-08-22 10:39:29 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,32 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
-}
 
-function ternaryCheckCity(){
-  // Write your code here!
+function scuberGreetingForFeet(feetValue){
+  if (feetValue>2500){
+    return "No can do."
 }
-
-function switchOnCharmFromTip(){
-  // Write your code here!
+else if (feetValue >2000){
+  return "I will gladly take your thirty bucks."
 }
+else if (feetValue<=400){
+  return "This one is on me!"
+}
+}
+function ternaryCheckCity(city){
+    if ( city=== 'NYC') {
+      return 'Ok, sounds good.' ;
+    }
+    else (city === 'Pittsburgh')
+      return 'No go.';
+    }
+function switchOnCharmFromTip(tip){
+  switch (tip){
+    case 'generous':
+      return'Thank you so much.';
+      break;
+      case 'not as generous':
+        return 'Thank you.';
+        break; 
+        default:
+          return'Bye.';
+          break;
+        }
+      }
